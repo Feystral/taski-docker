ca4c41b9b671f006a4143159cc53828692c193f4
Igor Kosov <feystral@gmail.com>
2025-04-16 11:33:06 +0500

    backend edit2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://feyeducation.ddns.net/',
+    'feyeducation.ddns.net',
     '89.169.163.172',
     '127.0.0.1:8080',
     'localhost'
